fc9b78270e1c3317399443bfe8990ae766aad6f6
Raji Ayinla <rajiayinla858@gmail.com>
2018-05-08 17:19:32 -0400

    make website 90 percent responsive

--- assets/js/scroller.js
+++ assets/js/scroller.js
@@ -1,5 +1,19 @@
  $(document).ready(function(){
 
+ 	var $burger    = $('.box-burger');
+ 	var $cross     = $('.box-cross');
+ 	var $hiddenbar = $('.hiddenbar');
+ 	var $container = $('.fp-container');
+ 
+    
+ 	$burger.on('click', function(){
+ 	  $hiddenbar.css({right:'0px', display:'block'});
+ 	});
+
+ 	$cross.on('click', function(){
+      $hiddenbar.css({right: '-500px', display: 'none'});	
+ 	});
+
  	 
  });
     
