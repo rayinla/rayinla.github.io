c3b1d1a4ae4d8b193766a0ff9651a63e8e9c3a40
Raji Ayinla <rajiayinla858@gmail.com>
2020-03-29 11:23:34 -0400

    Update scroller.js

--- assets/js/scroller.js
+++ assets/js/scroller.js
@@ -18,18 +18,6 @@
   let bucketWidths = ['0%', '50%'];
   let scrollCache;
 
-
-  function bodyOrHtml(){
-  	if ('scrollingElement' in document) {
-  		return document.scrollingElement;
-  	}
-  	// Fallback for legacy browsers
-  	if (navigator.userAgent.indexOf('WebKit') != -1) {
-  		return document.body;
-  	}
-  	return document.documentElement;
-  }
-
  	$burger.on('click', function(){
  	  $hiddenbar.css({right:'0px'});
  	});
@@ -39,51 +27,5 @@
  	});
 
 
-
-  //Classic View: toggling code and content
-
-  $codeSwitch.on('click', function(){
-     scrollCache = bodyOrHtml().scrollTop; // save current position
-     bodyOrHtml().scrollTop = 0;
-     $contentBucket.css({display: 'none'});
-     $codeBucket.css({display: 'flex', 'margin-top' : '20px'});
-     $stickyNav.css({position: 'relative'});
-     $menu.css({display: 'block'});
-
-  });
-  $contentSwitch.on('click', function(){
-     $contentBucket.css({display: 'block'});
-     bodyOrHtml().scrollTop = scrollCache;
-     $codeBucket.css({display: 'none'});
-     $stickyNav.css({position: 'fixed'});
-     $menu.css({display: 'none'});
-  });
-
-
-
-  //Reset height based on resize
-
-  $(window).on('resize', function(){
-    if (window.matchMedia("(min-width: 771px)").matches){
-        $content.css({height: '100%'});
-    } else if (window.matchMedia("(max-width: 771px)").matches){
-        $content.css({height: '33%'});
-    }
-  });
-
-  $resizeHeight.on('click', function(){
-    toggle(contentHeights);
-    $content.css({height: contentHeights[0]});
-  });
-
-  $resizeWidth.on('click', function(){
-    toggle(contentWidths);
-    $content.css({width: contentWidths[0]});
-  });
-
-
-  function toggle(arr){
-    arr.reverse();
-  }
 
  });
