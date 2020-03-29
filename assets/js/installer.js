a4a5a5096a5021925ea14cb0589ed77faa088842
Raji Ayinla <rajiayinla858@gmail.com>
2020-03-29 09:36:50 -0400

    Update installer.js

--- assets/js/installer.js
+++ assets/js/installer.js
@@ -1,11 +1,11 @@
-(function(){
+// (function(){
 
-  let deferredPrompt;
+//   let deferredPrompt;
 
-  window.addEventListener('beforeinstallprompt', (e) => {
-  // Prevent Chrome 67 and earlier from automatically showing the prompt
-    e.preventDefault();
-  // Stash the event so it can be triggered later.
-    deferredPrompt = e;
-  });
-})()
+//   window.addEventListener('beforeinstallprompt', (e) => {
+//   // Prevent Chrome 67 and earlier from automatically showing the prompt
+//     e.preventDefault();
+//   // Stash the event so it can be triggered later.
+//     deferredPrompt = e;
+//   });
+// })()
